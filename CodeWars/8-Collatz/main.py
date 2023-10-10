f109a01fdf620ce3632abec039d44f1c25477ee0
Pau Giuli <pau@giuli.cat>
2023-10-10 10:08:34 +0200

    Finished Collatz

---
+++ CodeWars/8-Collatz/main.py
@@ -0,0 +1,21 @@
+initialnum = int(input('Enter the initial number (n>1): '))
+
+num = initialnum
+
+steps = 0
+
+string = ''
+
+while num != 1:
+    string += f'{num} -> '
+    if num % 2 == 0:
+        num = round(num/2)
+    elif num % 2 == 1:
+        num = num * 3 + 1
+    steps += 1
+
+string += f'1 [{steps}]'
+
+print(string)
+    
+    
