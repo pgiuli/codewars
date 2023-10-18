3e7e02d64f6caa2bfaba4ebfcce7908be1fe6237
Pau Giuli <pau@giuli.cat>
2023-10-18 09:39:48 +0200

    Migrated to built-in methods, and now working

--- CodeWars/16-Acronimizer/main.py
+++ CodeWars/16-Acronimizer/main.py
@@ -1,23 +1,32 @@
 acronyms = [
-    (('National', 'Aeronautics', 'Space', 'Administration'), 'NASA'),
-    (('United', 'States'), 'US'),
-    (('Code', 'Wars'), 'CW'),
-    (('Hewlett', 'Packard'), 'HP')
+    ('National Aeronautics Space Administration', 'NASA'),
+    ('United States', 'US'),
+    ('Code Wars', 'CW'),
+    ('Hewlett Packard', 'HP')
     ]  
 
-initial_phrase = input('Enter the phrase: ')[:-1]
-print(initial_phrase)
-words = initial_phrase.split(' ')
-print(words)
+phrase = input('Enter the phrase: ')
+#print(phrase)
 
-for word in words:
-    for touple in acronyms:
-        if word in touple[0]:
-            for inword in touple[0]:
-                index = words.index(inword)
-                words.pop(index)
-            words.insert(index,touple[1])
+#words = phrase.split(' ')
+#print(words)
 
-print(words)
-final_phrase = ' '.join(words)
-print(f'{final_phrase}.')
+for touple in acronyms:
+    if touple[0] in phrase:
+        phrase = phrase.replace(touple[0], touple[1])
+
+
+
+#Don't look at me weird, it works.
+#for word in words:
+#    for touple in acronyms:
+#        if word in touple[0]:
+#            for inword in touple[0]:
+#                index = words.index(inword)
+#                words.pop(index)
+#            words.insert(index,touple[1])
+
+
+#print(words)
+
+print(phrase)
